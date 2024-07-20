f40cf7ae81cfee90e83ed62cd68bc6f829856ed5
Elif Maralı <elifmarali00@gmail.com>
2024-07-21 02:23:06 +0300

    unnecessary files were deleted

--- MultipleSliceCourseShoppingApp/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { formReducer } from "./Slice/formSlice"
-import { courseReducer } from "./Slice/courseSlice"
-export const store = configureStore({
-    reducer: {
-        form: formReducer,
-        course: courseReducer
-    }
-})
