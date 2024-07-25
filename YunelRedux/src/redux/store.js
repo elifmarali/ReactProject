e39ffea416f3276331cdc0f1d490859ee1c07c88
Elif Maralı <71133148+elifmarali@users.noreply.github.com>
2024-07-26 01:37:25 +0300

    Delete YunelRedux directory

--- YunelRedux/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartSlice from './Card/cardSlice';
-
-export const store = configureStore({
-    reducer: {
-        cart: cartSlice
-    }
-})
-console.log(cartSlice);
